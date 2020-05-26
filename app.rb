9a7ee8695a3864232e2589663ac77b9fbbf1b7bc
Willie Cadete <williecadete@gmail.com>
2020-05-26 12:14:30 +0100

    Change fullfiment field

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
 
     content_type :json, charset: 'utf-8'
     {
-      "fulfillmentMessages": response,
+      "fulfillment": response,
       "payload": {
         "telegram": {
           "text": response,
