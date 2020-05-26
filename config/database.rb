4ff3ab82f1cdd7e35c9e0621896cbc86298c3adc
Willie Cadete <williecadete@gmail.com>
2020-05-26 10:18:48 +0100

    Update database settings to use with Heroku

--- config/database.rb
+++ config/database.rb
@@ -23,13 +23,15 @@ configure :development do
 end
 
 configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/chatbot_production')
+
   set :database, {
     adapter: 'postgresql',
     encoding: 'utf8',
-    database: 'chatbot_production',
+    database: db.path[1..-1],
     pool: 5,
-    username: 'postgres',
-    password: ENV['DB_PASSWORD'],
-    host: 'postgres'
+    username: db.user,
+    password: db.password,
+    host: db.host
   }
 end
