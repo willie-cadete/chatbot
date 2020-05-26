40727efeff90f447f82e3e53ff788d409a389010
Willie Cadete <williecadete@gmail.com>
2020-05-26 10:16:18 +0100

    Add the route webhook route

--- app.rb
+++ app.rb
@@ -8,7 +8,26 @@ require './config/database'
 Dir["./app/models/*.rb"].each {|file| require file }
 
 class App < Sinatra::Base
-  get '/' do
-    'Hello World!'
+  post '/webhook' do
+    request.body.rewind
+    result = JSON.parse(request.body.read)["queryResult"]
+
+    if result["contexts"].present?
+      response = InterpretService.call(result['action'], result['contexts'][0]['parameters'])
+    else
+      response = InterpretService.call(result['action'], result['parameters'])
+    end
+
+    content_type :json, charset: 'utf-8'
+    {
+      "fullfillmentText": response,
+      "payload": {
+        "telegram": {
+          "text": response,
+          "parse_mode": "Markdown"
+        }
+      }
+    }.to_json
   end
+
 end
